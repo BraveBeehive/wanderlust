281668b52bb9cd08dc99beaa2ed68f5c0a8d0d9a
Mike Luby <Michael@mLuby.com>
2014-09-09 14:56:00 -0700

    fixed azure deployment files 3

--- deploy.sh
+++ deploy.sh
@@ -125,7 +125,7 @@ echo 4. Run grunt
 if [ -e "$DEPLOYMENT_SOURCE/Gruntfile.js" ]; then  
   eval $NPM_CMD install grunt-cli  
   exitWithMessageOnError "installing grunt failed"  
-  ./node_modules/.bin/grunt #build lint test serve   
+  ./node_modules/.bin/grunt build lint serve   
   exitWithMessageOnError "grunt failed"  
 fi  
   
