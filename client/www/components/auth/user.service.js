28bc3fcd93ada5ef21a23542d4aa01fcdd48f079
Mike Luby <Michael@mLuby.com>
2014-09-12 14:18:29 -0700

    commented out signup and login breakage

--- client/www/components/auth/user.service.js
+++ client/www/components/auth/user.service.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('wanderlustApp')
+angular.module('starter')
   .factory('User', function ($resource) {
     return $resource('/api/users/:id/:controller', {
       id: '@_id'
