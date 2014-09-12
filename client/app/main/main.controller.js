6e344f8090cdd8387909e0d4971ab31af32f985c
gitSauced <ttan08@gmail.com>
2014-09-11 19:02:53 -0700

    returns the amount of tours based on city feature almost complete.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,10 +1,19 @@
 'use strict';
 
 angular.module('wanderlustApp')
-  .controller('MainCtrl', function ($scope, $state) {
 
-    $scope.navToToursByLocation = function() {
-      // Value of $scope.location can be found in tours' $stateParams
-      $state.go('tours', $scope.location);
-    };
-  });
+.service('getCityName', function($scope) {
+	this.location = $scope.location;
+})
+
+
+.controller('MainCtrl', function ($scope, $state, getCityName) {
+
+  $scope.navToToursByLocation = function() {
+  	// console.log($scope.location);
+  	getCityName.getLocation();
+  	// $scope.location = getCityName.getLocation();
+     // Value of $scope.location can be found in tours' $stateParams
+    $state.go('tours', $scope.location);
+  };
+});
--- client/app/tours/tours.controller.js
+++ client/app/tours/tours.controller.js
@@ -30,30 +30,75 @@ angular.module('wanderlustApp')
     };
   })
 
-  .factory('httpGET', function($http){
-    return {
-      getData: function(callback){
-        return $http({
-          method: 'GET',
-          url: '/api/tours'
-          }).success(function(data){
-            callback(data);
-          });
-      }
+
+
+  .service('httpGET', function($http){
+    this.count = 0;
+    this.getData = function(callback){
+      return $http({
+        method: 'GET',
+        url: '/api/tours'
+        }).success(function(data){
+          callback(data);
+        });
     };
+
+     
+    this.getToursCountByCity = function() {
+      var self = this;
+      $http({
+        method: 'GET',
+        url: '/api/tours'
+      }).success(function(data) {
+        for(var i = 0; i < data.length; i++) {
+          if(data[i].city === 'San Francisco') {
+          self.count++;
+          }
+        }
+      });
+    };
+  
   })
 
+  // .factory('httpGET', function($http, $location){
+  //   var results = {};
+  //   // results.count = 0;
+  //   results.getData = function(callback){
+  //     return $http({
+  //       method: 'GET',
+  //       url: '/api/tours'
+  //     }).success(function(data) {
+  //       callback(data);
+  //     });
+  //   };
+  //   results.getToursCountByCity = function(){
+  //     return $http({
+  //       method: 'GET',
+  //       url: '/api/tours'
+  //     }).success(function(data) {
+  //       console.log(data);
+  //     });
+  //   };
+  //   return results;
+  // })
+
   .controller('ToursCtrl', function ($scope, $location, $http, httpGET) {
-    
+
     httpGET.getData(function(data){
       $scope.tours = data;
+      // console.log('LOCATION HERE', $location);
       console.log('DATA HERE: ', $scope.tours);
     });
 
+      
+    $scope.toursCount = httpGET;
+    $scope.toursCount.getToursCountByCity();
+
     //route to tour on click
     $scope.selectedTour = function(){
+      console.log('clicked');
         $location.path('/tours/showtour');
     };
-
     $scope.myInterval = 5000;
   });
+
