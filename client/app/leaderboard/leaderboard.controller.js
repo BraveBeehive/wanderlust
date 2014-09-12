ebca5790165f91eece328f2a0676298801892d62
JonathanWarrick <jonathan.warrick@gmail.com>
2014-09-12 10:51:03 -0700

    Properly sorts and dynamically adds position value using $index angular variable to Leaderboard table. Adds zebra-striping for AWESOME EFFECT.

--- client/app/leaderboard/leaderboard.controller.js
+++ client/app/leaderboard/leaderboard.controller.js
@@ -4,31 +4,26 @@ angular.module('wanderlustApp')
   .controller('LeaderboardCtrl', function ($scope, Points) {
     $scope.users = [{
       // test user 1
-      position: 1,
       name: 'Jonathan',
       points: 100
     },
     {
       // test user 2
-      position: 3,
       name: 'Collin',
       points: 25
     },
     {
       // test user 3
-      position: 2,
       name: 'Tommy',
       points: 75
     },
     {
       // test user 4
-      position: 4,
       name: 'Luby',
       points: -100
     },
     {
       // test user 5
-      position: 5,
       name: 'Test User',
       points: Points.user.points
     }
