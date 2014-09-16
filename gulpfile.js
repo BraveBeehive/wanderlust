74bea24cff008b79078fb20d60c4ccbf20794f69
Mike Luby <Michael@mLuby.com>
2014-09-15 19:24:27 -0700

    Improved gulpfile so `gulp` runs scss, db, nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,18 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var child_process = require('child_process');
 
 var paths = {
   sass: ['./client/www/lib/scss/**/*.scss','!**/bower_components'],
   css: ['./client/www/app/**/*.css','!**/bower_components']
 };
 
-gulp.task('default', ['scss and css']);
+gulp.task('default', [
+  'db', 
+  'scss and css', 
+  'serve', 
+  'open']);
 
 gulp.task('scss and css', function(done) {
   gulp.src(['./client/www/lib/ionic/scss/ionic.app.scss','./client/www/lib/ionic/scss/ionic.scss'])
@@ -44,22 +49,41 @@ gulp.task('watch', function() {
     ['scss and css']);
 });
 
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
+gulp.task('db', function() {
+  child_process.exec('mongod', function(err,stdout,stderr){
+    console.log(stdout);
+  });
 });
 
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
+gulp.task('serve', function() {
+  child_process.exec('nodemon server/app.js', function(err,stdout,stderr){
+    console.log(stdout);
+  });
 });
+
+gulp.task('open', function() {
+  child_process.exec('open http://localhost:9000/', function(err,stdout,stderr){
+    console.log(stdout);
+  });
+});
+
+
+// gulp.task('install', ['git-check'], function() {
+//   return bower.commands.install()
+//     .on('log', function(data) {
+//       gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+//     });
+// });
+
+// gulp.task('git-check', function(done) {
+//   if (!sh.which('git')) {
+//     console.log(
+//       '  ' + gutil.colors.red('Git is not installed.'),
+//       '\n  Git, the version control system, is required to download Ionic.',
+//       '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+//       '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+//     );
+//     process.exit(1);
+//   }
+//   done();
+// });
