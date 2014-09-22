66b0bd186755f95447fae225d9be2c76cd47da51
JonathanWarrick <jonathan.warrick@gmail.com>
2014-09-21 17:47:44 -0700

    Updates blank tour creator issue for "naked tours" as well (empty search string).

--- server/api/city/city.controller.js
+++ server/api/city/city.controller.js
@@ -5,7 +5,9 @@ var Tour = require('../tour/tour.model');
 // Get all the tours in all the cities
 exports.index = function(req, res) {
   console.log('.index beginning');
-  Tour.find(function (err, tours) {
+  Tour.find()
+      .populate('author')
+      .exec(function (err, tours) {
     if(err) { return handleError(res, err); }
     return res.json(200, {city:'all cities', tours: tours});
   });
@@ -18,9 +20,6 @@ exports.byCity = function(req,res) {
   Tour.find({city: req.params.city_name.replace('-',' ')})
       .populate('author')
       .exec(function(err,tours) {
-        for (var i = 0; i < tours.length; i++) {
-          console.log(tours[i].author.name);
-        }
     if(err) {return handleError(res,err); }
     if(!tours) {return res.send(404);}
     return res.json(200,{city:req.params.city_name, tours: tours});
